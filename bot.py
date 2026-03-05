76c185ecbbb64a24a08b6742116cf34fd3152e38
Alternative <savvabelitski@gmail.com>
2026-03-05 12:16:57 +0300

    fix token from env

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
 def main():
-    app = ApplicationBuilder().token("8792765347:AAGHQs11twmxzwSdeFBkHiuRfR3kVavCVYQ").build()
+    app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     print("Бот запущен!")
